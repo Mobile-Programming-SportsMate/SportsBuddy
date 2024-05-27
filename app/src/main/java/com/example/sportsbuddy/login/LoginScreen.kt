6fcfdea03267e2a200678c2e64ab2b97ee9d2b4b
jaepar <pjh6183@naver.com>
2024-05-27 17:18:30 +0900

    재하

--- app/src/main/java/com/example/sportsbuddy/login/LoginScreen.kt
+++ app/src/main/java/com/example/sportsbuddy/login/LoginScreen.kt
@@ -1,11 +1,9 @@
 package com.example.sportsbuddy.login
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun LoginScreen() {
-    
+    //재하
 }
 
