d1d8fbfb37e22e22bf54fcea621610d2e27ab7b1
mingyuuu02 <100djr2323@naver.com>
2024-05-27 17:46:34 +0900

    init

---
+++ app/src/main/java/com/example/sportsbuddy/screen/Chatting.kt
@@ -0,0 +1,12 @@
+package com.example.sportsbuddy.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Chatting() {
+    Column {
+        Text(text = "hi")
+    }
+}
