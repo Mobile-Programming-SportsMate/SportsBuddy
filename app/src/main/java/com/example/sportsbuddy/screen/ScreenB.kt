c4d1af9b29bc0c1eaa8c0bdf60a580b94ecd130b
t1nm1ksun <minsuk07672@naver.com>
2024-06-03 17:45:17 +0900

    resolve conflict

--- app/src/main/java/com/example/sportsbuddy/screen/ScreenB.kt
+++ app/src/main/java/com/example/sportsbuddy/screen/ScreenB.kt
@@ -1,14 +1,12 @@
 package com.example.sportsbuddy.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,18 +14,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -37,16 +23,20 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -54,32 +44,31 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.sportsbuddy.R
-
-import androidx.navigation.NavController
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenB(navController: NavController) {
     var selectedTab by remember { mutableStateOf(0) }
+    val context = LocalContext.current
 
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf("Select") }
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("SPORTSBUDDY") }
             )
         },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /* TODO: Add action */ }) {
-                Text("+")
-            }
-        }
+//        floatingActionButton = {
+//            FloatingActionButton(onClick = { /* TODO: Add action */ }) {
+//                Text("+")
+//            }
+//        }
     ) {
         Column(
             modifier = Modifier
@@ -97,6 +86,69 @@ fun ScreenB(navController: NavController) {
                     onClick = { selectedTab = 1 },
                     text = { Text("팀 매칭") }
                 )
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .padding(bottom = 60.dp)
+                ) {
+                    Button(
+                        onClick = { expanded = true },
+                        modifier = Modifier
+                            .size(width = 50.dp, height = 50.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = colorResource(id = R.color.light_gray)
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp),
+                            tint = Color.White
+                        )
+                    }
+
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier
+                            .background(
+                                colorResource(id = R.color.white),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                    ) {
+                        DropdownMenuItem(onClick = {
+                            selectedOption = "개인"
+                            expanded = false
+                            Toast.makeText(context, "개인 메이트 등록", Toast.LENGTH_SHORT).show()
+                            navController.navigate("add_individual_matching")
+                        }
+                        ) {
+                            Text(
+                                text = "개인",
+                                modifier = Modifier.padding(8.dp),
+                                color = Color.Gray,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Medium
+                            )
+                        }
+                        DropdownMenuItem(onClick = {
+                            selectedOption = "팀"
+                            expanded = false
+                            Toast.makeText(context, "팀 메이트 등록", Toast.LENGTH_SHORT).show()
+                            navController.navigate("add_team_matching")
+
+                        }) {
+                            Text(
+                                text = "팀",
+                                modifier = Modifier.padding(8.dp),
+                                color = Color.Gray,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Medium
+                            )
+                        }
+                    }
+                }
             }
             when (selectedTab) {
                 0 -> PersonalMatchList(navController)
@@ -107,87 +159,6 @@ fun ScreenB(navController: NavController) {
 }
 
 @Composable
-fun ScreenB(navController: NavHostController) {
-    val context = LocalContext.current
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(
-            text = "매칭 페이지",
-            fontSize = 40.sp,
-            fontWeight = FontWeight.ExtraBold,
-            modifier = Modifier.align(Alignment.Center)
-        )
-
-        var expanded by remember { mutableStateOf(false) }
-        var selectedOption by remember { mutableStateOf("Select") }
-
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-                .padding(bottom = 60.dp)
-        ) {
-            Button(
-                onClick = { expanded = true },
-                modifier = Modifier
-                    .size(width = 50.dp, height = 50.dp)
-                    .clip(RoundedCornerShape(8.dp)),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = colorResource(id = R.color.light_gray)
-                )
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = null,
-                    modifier = Modifier.size(24.dp),
-                    tint = Color.White
-                )
-            }
-
-
-        DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier
-                    .background(
-//                        colorResource(id = R.color.theme_blue02),
-                        colorResource(id = R.color.white),
-                        shape = RoundedCornerShape(10.dp)
-                    )
-            ) {
-                DropdownMenuItem(onClick = {
-                    selectedOption = "개인"
-                    expanded = false
-                    Toast.makeText(context,"개인 메이트 등록",Toast.LENGTH_SHORT).show()
-                    navController.navigate("add_individual_matching")
-                }
-                ) {
-                    Text(
-                        text = "개인",
-                        modifier = Modifier.padding(8.dp),
-                        color = Color.Gray,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Medium
-                    )
-                }
-                DropdownMenuItem(onClick = {
-                    selectedOption = "팀"
-                    expanded = false
-                    Toast.makeText(context,"팀 메이트 등록",Toast.LENGTH_SHORT).show()
-                    navController.navigate("add_team_matching")
-
-                }) {
-                    Text(
-                        text = "팀",
-                        modifier = Modifier.padding(8.dp),
-                        color = Color.Gray,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Medium
-                    )
-                }
-            }
-        }
-    }
-}
 fun PersonalMatchList(navController: NavController) {
     LazyColumn {
         items(5) { index ->
