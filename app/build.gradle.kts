75d86b2e1df2f5d9fcbd9fc4935de6e9368c12ac
jaepar <pjh6183@naver.com>
2024-06-12 15:28:59 +0900

    ..
    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx:21.0.0")
     implementation ("com.google.firebase:firebase-auth:23.0.0")
     implementation ("com.google.firebase:firebase-database:21.0.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
